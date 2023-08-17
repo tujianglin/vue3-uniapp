238453220a420be37185e74aff5dbb92c471a9b9
tujianglin <jianglin.tu@wimisoft.com>
2023-08-17 15:59:18 +0800

    feat: 初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
+onLaunch(() => {
+  console.log('12App Launch')
+})
+onShow(() => {
+  console.log('App Show')
+})
+onHide(() => {
+  console.log('App Hide')
+})
+</script>
+<style></style>
