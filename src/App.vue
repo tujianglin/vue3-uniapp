9d0b3bc37c6125026bd281fc6812acc83c654c58
tujianglin <jianglin.tu@wimisoft.com>
2023-08-17 16:04:25 +0800

    feat: 路径

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
-onLaunch(() => {
-  console.log('12App Launch')
-})
-onShow(() => {
-  console.log('App Show')
-})
-onHide(() => {
-  console.log('App Hide')
-})
+  import { onLaunch, onShow, onHide } from '@dcloudio/uni-app';
+  onLaunch(() => {
+    console.log('12App Launch');
+  });
+  onShow(() => {
+    console.log('App Show');
+  });
+  onHide(() => {
+    console.log('App Hide');
+  });
 </script>
 <style></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import uni from '@dcloudio/vite-plugin-uni';
 
-export default defineConfig({
-  plugins: [uni()],
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
+
+export default defineConfig(({}) => {
+  return {
+    plugins: [uni()],
+    resolve: {
+      alias: [
+        {
+          find: /\/@\//,
+          replacement: pathResolve('src') + '/',
+        },
+        {
+          find: /\/#\//,
+          replacement: pathResolve('types') + '/',
+        },
+      ],
+    },
+  };
 });
