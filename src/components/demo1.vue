8fc7896ddb25c1004d61f1e9691e49835d468e94
tujianglin <jianglin.tu@wimisoft.com>
2023-08-18 09:12:59 +0800

    feat: 自动引入无效,待处理

--- src/components/demo1.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>111 </div>
-</template>
-<script lang="ts" setup>
-  import {} from 'vue';
-</script>
-<style lang="less" scoped></style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,46 +1,7 @@
 <template>
-  <view class="content w-200 h-100 bg-[red]" @click="handleClick">
-    {{ title }}
-    <Demo1 />
-  </view>
+  <view> 1112322 <uni-badge text="1" /> </view>
 </template>
 
-<script lang="ts" setup>
-  import { ref } from 'vue';
-  import Demo1 from '/@/components/demo1.vue';
-  import { useAppStore } from '/@/store/modules/app';
-  const appStore = useAppStore();
-  const title = ref('Hello');
-  const router = useRouter();
-  const handleClick = () => {
-    console.log(appStore.a);
-    router.push({
-      path: '/pages/hello/index',
-    });
-  };
-</script>
+<script lang="ts" setup></script>
 
-<style lang="scss" scoped>
-  .content {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-  }
-
-  .logo {
-    margin: 200rpx auto 50rpx;
-    width: 200rpx;
-    height: 200rpx;
-  }
-
-  .text-area {
-    display: flex;
-    justify-content: center;
-  }
-
-  .title {
-    font-size: 36rpx;
-    color: #8f8f94;
-  }
-</style>
+<style lang="scss" scoped></style>
