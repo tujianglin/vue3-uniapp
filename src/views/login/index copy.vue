6c0e6db422fcadda9f1a85fb2bf65cb48e07a894
tujianglin <jianglin.tu@wimisoft.com>
2023-11-21 16:12:21 +0800

    feat: demo

---
+++ src/views/login/index copy.vue
@@ -0,0 +1,13 @@
+<script lang="tsx">
+  import { defineComponent } from 'vue';
+  import { Repl } from '@vue/repl';
+  // @ts-ignore
+  import MonacoEditor from '@vue/repl/monaco-editor';
+  import '@vue/repl/style.css';
+  export default defineComponent({
+    setup() {
+      return () => <Repl editor={MonacoEditor}></Repl>;
+    },
+  });
+</script>
+<style lang="less" scoped></style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,70 +1,8 @@
 <script lang="tsx">
-  import { defineComponent, reactive, ref } from 'vue';
-  import { InputProps, Button } from 'ant-design-vue';
-  import { BasicForm } from '/@/components/Form';
-  import { FormOption } from '/@/components/Form/src/types';
-  import { FormExpose } from 'ant-design-vue/es/form/Form';
-  import api from '/@/api';
-  import { CreateUserDto } from '/@/api/modules/data-contracts';
+  import { defineComponent } from 'vue';
   export default defineComponent({
     setup() {
-      const formRef = ref<FormExpose>();
-      const options: FormOption = {
-        colon: false,
-        layout: 'horizontal',
-        labelCol: {
-          span: 2,
-        },
-        columns: [
-          {
-            name: 'username',
-            component: 'Input',
-            label: '用户名',
-            componentProps: {
-              placeholder: '请输入用户名',
-            } as InputProps,
-            rules: [
-              {
-                required: true,
-                message: '请输入用户名',
-                trigger: ['blur'],
-              },
-            ],
-          },
-          {
-            name: 'password',
-            component: 'InputPassword',
-            label: '密码',
-            componentProps: {
-              placeholder: '请输入密码',
-            } as InputProps,
-            rules: [
-              {
-                required: true,
-                message: '请输入密码',
-                trigger: ['blur'],
-              },
-            ],
-          },
-        ],
-      };
-      const formState = reactive({
-        username: '',
-        password: '',
-      });
-      const handleSubmit = () => {
-        formRef.value.validate().then((res: CreateUserDto) => {
-          api.authControllerSignIn(res).then((res) => {
-            console.log(res.data.data.password);
-          });
-        });
-      };
-      return () => (
-        <>
-          <BasicForm ref={formRef} data={formState} options={options}></BasicForm>
-          <Button onClick={handleSubmit}>登录</Button>
-        </>
-      );
+      return () => <div></div>;
     },
   });
 </script>
